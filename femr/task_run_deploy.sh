4e520639b1d6bc1751801477bed99722ee70c8db
Kevin Zurek <kevinzurek@gmail.com>
2017-04-30 20:31:16 -0400

    remove old volumes too

--- femr/task_run_deploy.sh
+++ femr/task_run_deploy.sh
@@ -6,6 +6,8 @@ COMMIT=$(cd git-femr-app && git rev-parse --short HEAD)
 
 OLD_INSTANCE_ID=$(aws ec2 describe-instances --filters Name=tag:ID,Values=fEMR-demo | jq -r .Reservations[].Instances[].InstanceId)
 
+OLD_VOLUME_ID=$(aws --profile concourse ec2 describe-instances --filter Name=tag:ID,Values=fEMR-demo | jq -r .Reservations[].Instances[].BlockDeviceMappings[].Ebs.VolumeId)
+
 aws ec2 create-tags --resources $OLD_INSTANCE_ID --tags Key=ID,Value=fEMR-demo-old
 
 aws ec2 run-instances --image-id ami-ca6cf5dc --count 1 --instance-type t2.small --key-name concourse_id_rsa --security-group-ids sg-38b54e44 --subnet-id subnet-1b91f540 --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value='$COMMIT'},{Key=ID,Value=fEMR-demo}]' --user-data file://git-concourse-app/femr/deploy_server_config.sh > output.txt
@@ -17,4 +19,8 @@ aws ec2 wait instance-status-ok --instance-ids $NEW_INSTANCE_ID
 aws ec2 associate-address --instance-id $NEW_INSTANCE_ID --allocation-id eipalloc-266cdf18
 
 aws ec2 terminate-instances --instance-ids $OLD_INSTANCE_ID
+
+aws ec2 wait volume-available --volume-ids $OLD_VOLUME_ID
+
+aws ec2 delete-volume --volume-id $OLD_VOLUME_ID
 
